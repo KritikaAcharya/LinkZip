060bc8557462e293208e21d592d392bb8c4ea2e4
Kritika Acharya <acharyakritika44@gmail.com>
2024-07-01 18:20:49 +0545

    mvcmapped

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const app=express();
+const urlRoute=require("./routes/url")
+const {connectToMongoDB}=require("./connect")
 
 PORT=8001;
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(()=>console.log("mongodb connected"))
+
+app.use("/url",urlRoute);
 
 app.listen(PORT,()=>{
     console.log(`app is running in port : ${PORT}`)
